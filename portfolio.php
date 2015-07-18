720c147bad9d53d0651c8434f779839bf49e7de0
Oscar Ferraro <oscar@oferraro.com.ar>
2015-07-18 10:49:24 -0300

    first commit

---
+++ portfolio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+	<title>Gallery</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+	
+	<section>
+		<h1>Portfolio</h1>
+		<article>
+			<figure>
+				<a href="galeria/grandes/auricular1.jpg">
+					<img src="galeria/auricular1.jpg" alt="auricular">
+				</a>
+			</figure>
+			<h2>Auricular</h2>
+			<div>Electronica </div>
+		</article>
+	</section>
+	
+</body>
+
+</html>
