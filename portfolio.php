ac7e45d61b7e13bbc3c0da14bbf1a8b0eda91833
Oscar Ferraro <oscar@oferraro.com.ar>
2015-07-18 12:35:44 -0300

    Add more images with PHP

--- portfolio.php
+++ portfolio.php
@@ -4,6 +4,7 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 	<title>Gallery</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 
@@ -11,17 +12,28 @@
 	
 	<section>
 		<h1>Portfolio</h1>
-		<article>
-			<figure>
-				<a href="galeria/grandes/auricular1.jpg">
-					<!-- :before -->
-					<img src="galeria/auricular1.jpg" alt="auricular">
-					<!-- :after -->
-				</a>
-			</figure>
-			<h2>Auricular</h2>
-			<div>Electronica </div>
-		</article>
+<?php
+	$dirName = 'galeria';
+	$d = opendir($dirName);
+	$ignore = array('.', '..');
+	while ($f = readdir($d)):
+		if (!in_array($f, $ignore) && !is_dir($dirName.'/'.$f)): ?>
+			<article>
+				<figure>
+					<a href="galeria/grandes/<?php echo $f; ?>">
+						<!-- :before -->
+						<img src="galeria/<?php echo $f; ?>" alt="<?php echo $f;?>">
+						<!-- :after -->
+					</a>
+				</figure>
+				<h2><?php echo $f;?></h2>
+				<div>Electronica </div>
+			</article>
+<?php		
+		endif;
+	endwhile;
+?>
+		
 	</section>
 	
 </body>
